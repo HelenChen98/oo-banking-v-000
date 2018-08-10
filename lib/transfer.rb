058e9c4ce7fb867afb8072995625cf106d2216fe
Karen C <chenyanhuan66@163.com>
2018-08-10 09:30:09 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,10 +16,13 @@ class Transfer
     if !@sender.valid?
       return "Transaction rejected. Please check your account balance."
     end
-    if @status == "pending" && @sender.valid?
+    if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
     end
   end
 
